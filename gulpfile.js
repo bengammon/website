a13965396bf85566b74aa4a309fce8829ee8b16c
Peter Keating <peter@moov2.com>
2014-03-21 12:39:35 +0000

    added gulp task for compiling Sass to CSS and started piecing together the stylesheet.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
 var gulp = require('gulp');
 
 // include plugins.
-var connect = require('gulp-connect');
+var connect = require('gulp-connect'),
+    sass = require('gulp-ruby-sass');
 
 // spins up a local web server
 gulp.task('connect', connect.server({
@@ -10,5 +11,18 @@ gulp.task('connect', connect.server({
     host: '*'
 }));
 
+// compiles Sass into CSS.
+gulp.task('styles', function () {
+    gulp.src('src/css/styles.scss')
+        .pipe(sass({ sourcemap: false }))
+        .pipe(gulp.dest('src/css/'));
+});
+
+// set up files that should be watched for changes that trigger a task to run.
+gulp.task('watch', function() {
+    // changes to Sass files should run the Sass to CSS compilation task
+    gulp.watch('src/css/**/*.scss', ['styles']);
+});
+
 // default task is to be used when developing.
-gulp.task('default', ['connect']);
+gulp.task('default', ['styles', 'connect', 'watch']);
